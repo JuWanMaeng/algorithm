4d28efefebdc75af8905174ff440d0ad3e1b21dc
Juwan Maeng <63851856+JuWanMaeng@users.noreply.github.com>
2020-11-13 00:12:06 +0900

    1113

--- 문자열/리스트형 딕셔너리(해쉬),sorted함수,join함수 활용.py
+++ 문자열/리스트형 딕셔너리(해쉬),sorted함수,join함수 활용.py
@@ -4,7 +4,7 @@ class Solution:
     def groupAnagrams(self, strs):
         d=collections.defaultdict(list)
         for words in strs:
-            d[''.join(sorted(words))].append(words)           #sorted 함수는 문자열도 리스트로 반환한다 ex) 'bca'->['a','b','c']  -->join 함수로 다시 'abc' 로 바꾸기 가능
+            d[''.join(sorted(words))].append(words)           
         
         return d.values()
 
